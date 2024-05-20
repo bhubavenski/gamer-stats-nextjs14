9d5feba7ed12f9701d6ba523943aa436a9c524e9
Versus Development <bockster6669@gmail.com>
2024-05-20 23:28:14 +0300

    created header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, League_Spartan } from 'next/font/google';
 import './globals.css';
 import React from 'react';
+import Header from '@/components/shared/header.server';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -28,11 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${spartan.variable}`}>
-        <header>KO staa</header>
-        <main className="font-spartan">
-          {children}
-        </main>
-        <footer>brat</footer>
+        <Header />
+        <main className="font-spartan">{children}</main>
       </body>
     </html>
   );
