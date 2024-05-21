5b7e9ba2061a08bd16fc2e6da4a74b3631c68209
Versus Development <bockster6669@gmail.com>
2024-05-21 20:15:51 +0300

    added Reackk Hook Form

--- components/game-library/sidebar/searchBar.server.tsx
+++ components/game-library/sidebar/searchBar.server.tsx
@@ -1,16 +1,37 @@
+// TODO: Use React Hook Form
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function SearchBar() {
+  // <div className="flex w-[227px] rounded-md border border-[#26333F] bg-[#030E1A] p-2">
+  //   <input className=' bg-inherit'/>
+  //   <Image
+  //     src="/icons/search.svg"
+  //     alt="search for game"
+  //     width={24}
+  //     height={24}
+  //   />
+  // </div>
+
+  const { register } = useForm();
+
   return (
-    <div className="flex w-[227px] rounded-md border border-[#26333F] bg-[#030E1A] p-2">
-      <input />
-      <Image
-        src="/icons/search.svg"
-        alt="search for game"
-        width={24}
-        height={24}
-      />
-    </div>
+    <form
+      onSubmit={() => {}}
+      className="flex w-[227px] gap-2 rounded-md border border-[#26333F] bg-[#030E1A] p-2"
+    >
+      <input {...register('gameName')} className='bg-inherit outline-none'/>
+      <button type="submit">
+        <Image
+          src="/icons/search.svg"
+          alt="search for game"
+          width={24}
+          height={24}
+        />
+      </button>
+    </form>
   );
 }
