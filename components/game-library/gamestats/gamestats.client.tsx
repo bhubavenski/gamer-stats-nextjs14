7266fcc6eb4ce0c347a6b01dfe7116b67f186065
AstrO <kostadinov226@abv.bg>
2024-05-22 19:58:48 +0300

    Finished gameStats Imported icons correctly Added icons

--- components/game-library/gamestats/gamestats.client.tsx
+++ components/game-library/gamestats/gamestats.client.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import gameBanner from '../../../public/images/gamebanner.png';
-import righticon from '../../../public/icons/righticon.png';
-import heart from '../../../public/icons/Heart.png';
 
 export default function GameStats() {
     return (
         <div className='relative h-[480px] w-full'>
             <Image
-                src={gameBanner}
+                src='/images/gamebanner.png'
                 alt='Game Banner'
                 layout='fill'
                 quality={100}
@@ -16,13 +13,36 @@ export default function GameStats() {
             />
             <div className='absolute bottom-4 left-4 space-y-6 p-6'>
                 <p className='text-4xl font-bold'>Forza Horizon 5</p>
-                <button className='flex w-[162px] items-center rounded bg-[#5653FF] px-4 py-2'>
+                <button className='flex w-[162px] rounded bg-[#5653FF] px-7 py-2'>
                     <span className='mr-4'>View Stats</span>
-                    <Image src={righticon} alt='icon' width={20} height={20} />
+                    <Image src='/icons/righticon.png' alt='icon' width={20} height={20} />
                 </button>
             </div>
             <div className='absolute right-4 top-4 p-4'>
-                <Image src={heart} alt='heart icon' width={29.09} height={29.09} />
+                <Image src='/icons/Heart.png' alt='heart icon' width={29.09} height={29.09} />
+            </div>
+            <div className='absolute bottom-4 right-4 flex space-x-8 px-8'>
+                <div className='flex items-center space-x-2'>
+                    <Image src='/icons/time.png' width={28} height={32} alt='Time icon' />
+                    <div className='flex flex-col items-start'>
+                        <p>Total PlayTime</p>
+                        <p className='text-sm'>50 hours</p>
+                    </div>
+                </div>
+                <div className='flex items-center space-x-2'>
+                    <Image src='/icons/achiviement.png' width={28} height={32} alt='Achievements icon' />
+                    <div className='flex flex-col items-start'>
+                        <p>Achievements</p>
+                        <p className='text-sm'>12/100</p>
+                    </div>
+                </div>
+                <div className='flex items-center space-x-2'>
+                    <Image src='/icons/friends.svg' width={28} height={32} alt='Friends icon' />
+                    <div className='flex flex-col items-start'>
+                        <p>Friends</p>
+                        <p className='text-sm'>15 friends</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
