b9960542bbcae3b6ee1eb7c0786a3cbd9e4dd2eb
Versus Development <bockster6669@gmail.com>
2024-05-27 15:30:14 +0300

    update

--- components/game-library/sidebar/game-lib/GameLib_Carousel.client.tsx
+++ components/game-library/sidebar/game-lib/GameLib_Carousel.client.tsx
@@ -1,38 +1,68 @@
 'use client';
 
 import React from 'react';
-import {
-  PrevButton,
-  NextButton,
-  usePrevNextButtons,
-} from '../../../shared/carousel/CarouselArrowButtons.client';
-import useEmblaCarousel from 'embla-carousel-react';
 import '@/styles/embla.css';
-import CarouselViewPort from '@/components/shared/carousel/CarouselViewPort';
-import CarouselContainer from '@/components/shared/carousel/CarouselContainer';
 import MostPlayedCard from '@/components/cards/MostPlayedCard.server';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from '@/components/ui/carousel';
 
 export function GameLib_Carousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ dragFree: true });
-
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick,
-  } = usePrevNextButtons(emblaApi);
-
   return (
-    <div className="relative">
-      <CarouselViewPort emblaRef={emblaRef}>
-        <CarouselContainer>
-          <MostPlayedCard />
-        </CarouselContainer>
-      </CarouselViewPort>
-
-      <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+    <div className="border-2 border-yellow-500">
+      <Carousel
+        opts={{
+          dragFree: true,
+        }}
+        orientation="vertical"
+        className="max-h-[600px] min-h-[600px] max-w-xs  overflow-hidden border-2 border-red-500"
+      >
+        <CarouselContent className="-mt-1 border-2 border-blue-500">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <CarouselItem key={index} className="">
+              <MostPlayedCard />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
+  );
+}
+
+/* <CarouselViewPort emblaRef={emblaRef}>
+  <CarouselContainer>
+    <MostPlayedCard />
+  </CarouselContainer>
+</CarouselViewPort>
+
+<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} /> */
+
+export function CarouselOrientation() {
+  return (
+    <Carousel
+      opts={{
+        dragFree: true,
+      }}
+      orientation="vertical"
+      className="max-h-full max-w-xs  border-2 border-red-500"
+    >
+      <CarouselContent className="-mt-1 max-h-full border-2 border-blue-500">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="">
+            <MostPlayedCard />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
 
--- components/game-library/sidebar/sidebar.server.tsx
+++ components/game-library/sidebar/sidebar.server.tsx
@@ -45,9 +45,10 @@ export default function Sidebar() {
 
       <hr className=" border-t-2 border-t-line_devider" />
 
-      <section>
-        GameLibNav
-        <GameLib_Carousel/>
+      <section className=" flex max-h-[630px] min-h-[630px] flex-col  border-2 border-black bg-most_played_sec_bg">
+        <h3>GameLibNav</h3>
+        
+        <GameLib_Carousel />
       </section>
     </div>
   );
