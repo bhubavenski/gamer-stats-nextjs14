857d35b781d49fd722f412c4405db02ffde2b6bb
Versus Development <bockster6669@gmail.com>
2024-06-03 00:52:51 +0300

    change function name and rendering technique

--- components/game-library/gamestats/GameBanner.server.tsx
+++ components/game-library/gamestats/GameBanner.server.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function GameStats() {
+export default function GameBanner() {
     return (
         <div className='relative h-[480px] w-full'>
             <Image
